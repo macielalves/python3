60f525a6a038bd51328fa5c43cfe134ce21ba75a
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-04-09 14:52:01 -0300

    update sem08t2q5

--- SEM08/SEM08T2/SEM08T2Q5.py
+++ SEM08/SEM08T2/SEM08T2Q5.py
@@ -1,8 +1,14 @@
-def media_final(nt1=0, nt2=0, nt3=0, mde=0):
-    return float(f'{(nt1 + nt2*2 + nt3*3 + mde) / 7 :.2f}')
+def media_final(nt1, nt2=0, nt3=0, mde=0):
+    mf = (nt1 + nt2*2 + nt3*3 + mde) / 7
+    if mf > 10:
+        mf = 10
+    elif mf < 0:
+        mf = 0
+    return mf
 
 
-def conceito(mf=0):
+def conceito(mf):
+    mf = float(mf)
     if mf >= 9.0:
         return 'A'
     elif mf >= 7.5 < 9.0:
@@ -15,7 +21,8 @@ def conceito(mf=0):
         return 'E'
 
 
-def estatus(txt='Estude!'):
+def estatus(txt):
+    txt = str(txt).strip()
     if txt == 'A' or txt == 'B' or txt == 'C':
         return 'Aprovado'
     elif txt == 'D' or txt == 'E':
@@ -23,14 +30,14 @@ def estatus(txt='Estude!'):
 
 
 n_matricula = input().strip()
-nota1 = float(input())# 1º nota
-nota2 = float(input())# 2º nota
-nota3 = float(input())# 3º nota
-media_exercicios = float(input())
+nota1 = float(input().strip())# 1º nota
+nota2 = float(input().strip())# 2º nota
+nota3 = float(input().strip())# 3º nota
+media_exercicios = float(input().strip())
 mf = media_final(nota1, nota2, nota3, media_exercicios)
 conceito = conceito(mf)
 
 print(n_matricula)
-print(mf)
+print(f'{mf:.2f}')
 print(conceito)
 print(estatus(conceito))
