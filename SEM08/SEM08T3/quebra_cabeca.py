9863d18db2186d718f5362aed41fe0bad30f4cf0
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-04-09 20:50:34 -0300

    Remove main.py e add quebra_cabeça

--- SEM08/SEM08T3/quebra_cabeca.py
+++ SEM08/SEM08T3/quebra_cabeca.py
@@ -1,5 +1,40 @@
-from unicodedata import normalize
-import urllib.request, ssl
+def remover_acentos(texto):
+    from unicodedata import normalize
+    texto = str(texto)
+    return normalize('NFKD', texto).encode('ASCII', 'ignore').decode('ASCII')
+
+
+def horoscopo(signo_desejado):
+    import urllib.request, ssl
+    signo_formatado = remover_acentos(signo_desejado).lower()
+    minha_url = 'https://www.horoscopovirtual.com.br/horoscopo/' + signo_formatado
+
+    requisicao = urllib.request.Request(
+        url=minha_url,
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    contexto_ssl = ssl.create_default_context()
+    contexto_ssl.check_hostname = False
+    contexto_ssl.verify_mode = ssl.CERT_NONE
+
+    resposta = urllib.request.urlopen(requisicao, context=contexto_ssl)
+    pagina = resposta.read().decode('utf-8')
+    marcador_inicio = '<p class="text-pred">'
+    marcador_final = '<a class="webshare-link">Compartilhar</a>'
+
+    inicio = pagina.find(marcador_inicio) + len(marcador_inicio)
+    final = pagina.find(marcador_final, inicio)
+
+    return signo_desejado + ': ' + pagina[inicio:final].strip()
+
+
+def separar_data(dma):
+    a = dma % 10000
+    dma //= 10000
+    m = dma % 100
+    dma //= 100
+    d = dma
+    return d, m, a
 
 
 def signo(dia, mes):
@@ -29,29 +64,6 @@ def signo(dia, mes):
         return 'Aquário' if dia < 19 else 'Peixes'
 
 
-def horoscopo(signo_desejado):
-    signo_formatado = remover_acentos(signo_desejado).lower()
-    minha_url = 'https://www.horoscopovirtual.com.br/horoscopo/' + signo_formatado
-
-    requisicao = urllib.request.Request(
-        url=minha_url,
-        headers={'User-Agent': 'Mozilla/5.0'}
-    )
-    contexto_ssl = ssl.create_default_context()
-    contexto_ssl.check_hostname = False
-    contexto_ssl.verify_mode = ssl.CERT_NONE
-
-    resposta = urllib.request.urlopen(requisicao, context=contexto_ssl)
-    pagina = resposta.read().decode('utf-8')
-    marcador_inicio = '<p class="text-pred">'
-    marcador_final = '<a class="webshare-link">Compartilhar</a>'
-
-    inicio = pagina.find(marcador_inicio) + len(marcador_inicio)
-    final = pagina.find(marcador_final, inicio)
-
-    return signo_desejado + ':' + pagina[inicio:final].strip()
-
-
 def main():
     # Entrada de dados
     nascimento = int(input('Digite sua data de nascimento no formato DDMMAAAA: '))
@@ -63,20 +75,6 @@ def main():
 
     # Saída de dados
     print(horoscopo_de_hoje)
-
-
-def remover_acentos(texto):
-    texto = str(texto)
-    return normalize('NFKD', texto).encode('ASCII', 'ignore').decode('ASCII')
-
-
-def separar_data(dma):
-    a = dma % 10000
-    dma //= 10000
-    m = dma % 100
-    dma //= 100
-    d = dma
-    return d, m, a
 
 
 if __name__ == '__main__':
