6a2e76ef3e5b529739e2f748a75f9ab12e98d7a3
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-05-10 20:48:04 -0300

    add vinte_e_um.py

--- OUTROS_PROJETOS/Porta_da_fortuna/vinte_e_um.py
+++ OUTROS_PROJETOS/Porta_da_fortuna/vinte_e_um.py
@@ -4,33 +4,36 @@ from random import randint, seed
 # inicializa o gerador de números aleatórios
 seed()
 
-print('''
-Vinte e um!
-===========
-Tente fazer exatamente 21 pontos!
-''')
+def main():
+    print('''
+    Vinte e um!
+    ===========
+    Tente fazer exatamente 21 pontos!
+    ''')
 
-jogando = True
-pontos = 0
+    jogando = True
+    pontos = 0
+
+    while jogando:
+        # gera um número aleatório
+        numero = randint(1, 10)
+        pontos += numero
+        print("\nSeu próximo número é", numero)
+        print("Sua pontuação agora é", pontos)
+        # Condição para o usuário continuar ou não somando os números
+        print("\nGostaria de somar mais um número? (S/n)")
+        # recebe os dados do usuário
+        resposta = input().strip()
+        # processa a resposta, e se for n ele retorna false fechando o while
+        if resposta[0].lower() == 'n':
+            jogando = False
+
+    print("Sua pontuação final é", pontos)
+    if pontos == 21:
+        print("Parabéns, você VENCEU!!")
+    else:
+        print("Que pena!!")
 
 
-while jogando:
-    # gera um número aleatório
-    numero = randint(1, 10)
-    pontos += numero
-    print("\nSeu próximo número é", numero)
-    print("Sua pontuação agora é", pontos)
-    # Condição para o usuário continuar ou não somando os números
-    print("\nGostaria de somar mais um número? (S/n)")
-    # recebe os dados do usuário
-    resposta = input().strip()
-    # processa a resposta, e se for n ele retorna false fechando o while
-    if resposta[0].lower() == 'n':
-        jogando = False
-
-
-print("Sua pontuação final é", pontos)
-if pontos == 21:
-    print("Parabéns, você VENCEU!!")
-else:
-    print("Que pena!!")
+if __name__ == "__main__":
+    main()
